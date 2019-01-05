ece7d887b95f89db773d75b8dc0ea369ab8e37a8
Arnon Keereena <a.keereena@gmail.com>
2019-01-06 02:26:41 +0700

    Add required parameters and expose initialization

--- lib/domain/GithubDomain/GithubDomain/Classes/Entities/LoginRequest.swift
+++ lib/domain/GithubDomain/GithubDomain/Classes/Entities/LoginRequest.swift
@@ -7,4 +7,16 @@ import Foundation
 public struct LoginRequest {
   public var username: String
   public var password: String
+  public var scopes: [String]
+  public var note: String?
+  
+  public init(username: String,
+              password: String,
+              scopes: [String] = ["public_repo"],
+              note: String? = nil) {
+    self.username = username
+    self.password = password
+    self.scopes = scopes
+    self.note = note
+  }
 }
