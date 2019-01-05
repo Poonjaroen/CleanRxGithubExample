6d46a5046afc00617a4c4fa6397ec78d1205e08f
Arnon Keereena <a.keereena@gmail.com>
2019-01-06 01:05:23 +0700

    Install pods

---
+++ CleanRxGithub/Pods/Target Support Files/GithubNetworkPlatform/GithubNetworkPlatform-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GithubNetworkPlatform : NSObject
+@end
+@implementation PodsDummy_GithubNetworkPlatform
+@end
--- lib/domain/GithubDomain/Example/Pods/Target Support Files/Pods-GithubDomain_Example/Pods-GithubDomain_Example-frameworks.sh
+++ lib/domain/GithubDomain/Example/Pods/Target Support Files/Pods-GithubDomain_Example/Pods-GithubDomain_Example-frameworks.sh
@@ -144,9 +144,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GithubDomain/GithubDomain.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GithubDomain/GithubDomain.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
