8d689254428a4c19358478277efe9a5125ab63dd
Arnon Keereena <a.keereena@gmail.com>
2019-01-06 10:33:52 +0700

    Adjust target to use new data model.

--- lib/utility/GithubNetwork/GithubNetwork/Classes/Target.swift
+++ lib/utility/GithubNetwork/GithubNetwork/Classes/Target.swift
@@ -6,7 +6,7 @@ import Foundation
 import Moya
 
 public enum Target: Moya.TargetType {
-  case login(username: String, password: String)
+  case login(request: LoginRequest)
   
   public var baseURL: URL {
     return URL(string: "https://api.github.com")!
@@ -14,7 +14,7 @@ public enum Target: Moya.TargetType {
   
   public var path: String {
     switch self {
-    case .login(let username, _): return "/users/\(username)"
+    case .login(let request): return "/users/\(request.username)"
     }
   }
   
@@ -27,13 +27,15 @@ public enum Target: Moya.TargetType {
   }
   
   public var task: Task {
-    return Task.requestPlain
+    switch self {
+    case .login(let request): return Task.requestJSONEncodable(request.body)
+    }
   }
   
   public var headers: [String: String]? {
     switch self {
-    case .login(let username, let password):
-      let credentials = "\(username):\(password)"
+    case .login(let request):
+      let credentials = "\(request.username):\(request.password)"
       let base64 = credentials.data(using: .utf8)?.base64EncodedString() ?? ""
       return ["Authorization": "Basic \(base64)"]
     }
