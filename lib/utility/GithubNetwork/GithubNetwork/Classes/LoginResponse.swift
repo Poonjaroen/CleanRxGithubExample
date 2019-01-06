b4e36d2edf841e587f92822ec477cbbde0d26778
Arnon Keereena <a.keereena@gmail.com>
2019-01-06 10:24:18 +0700

    Move login request & response entities to be a util target specific

--- lib/utility/GithubNetwork/GithubNetwork/Classes/LoginResponse.swift
+++ lib/utility/GithubNetwork/GithubNetwork/Classes/LoginResponse.swift
@@ -5,5 +5,15 @@
 import Foundation
 
 public struct LoginResponse {
-
+  public var id: String?
+  public var url: String?
+  public var scopes: [String]
+  public var token: String?
+  public var tokenLastEight: String?
+  public var hashedToken: String?
+  public var note: String?
+  public var noteUrl: String?
+  public var updatedAt: Date?
+  public var createdAt: Date?
+  public var fingerprint: String?
 }
