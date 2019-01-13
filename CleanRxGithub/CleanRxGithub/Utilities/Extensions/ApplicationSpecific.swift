c7c2cf10b9f65e60561ee7f3514fad377a136e9b
Arnon Keereena <a.keereena@gmail.com>
2019-01-13 18:56:35 +0700

    Add final touch

--- CleanRxGithub/CleanRxGithub/Utilities/Extensions/ApplicationSpecific.swift
+++ CleanRxGithub/CleanRxGithub/Utilities/Extensions/ApplicationSpecific.swift
@@ -9,23 +9,25 @@ import UIKit
 extension UITabBar {
   func toAppStyle() {
     shadowImage = .with(color: .clear)
-    backgroundImage = .with(color: .init(red: 0, green: 0, blue: 0, alpha: 0.5))
-    let blurEffect = UIBlurEffect(style: .light)
-    let blurEffectView = UIVisualEffectView(effect: blurEffect)
-    blurEffectView.frame = frame
-    blurEffectView.alpha = 0.5
-    addSubview(blurEffectView)
+    backgroundImage = .with(color: .init(red: 0.04, green: 0.30, blue: 0.55, alpha: 0.9))
   }
 }
 
 extension UINavigationBar {
   func toAppStyle() {
-    setBackgroundImage(.with(color: .init(red: 0, green: 0, blue: 0, alpha: 0.5)), for: .default)
     shadowImage = .with(color: .clear)
+    setBackgroundImage(.with(color: .init(red: 0.1, green: 0.1, blue: 1, alpha: 0.3)), for: .default)
+    addBlur()
+  }
+}
+
+extension UIView {
+  func addBlur() {
     let blurEffect = UIBlurEffect(style: .light)
     let blurEffectView = UIVisualEffectView(effect: blurEffect)
-    blurEffectView.frame = frame
-    blurEffectView.alpha = 0.5
+    blurEffectView.frame = bounds
+    blurEffectView.alpha = 0.8
     addSubview(blurEffectView)
+    sendSubviewToBack(blurEffectView)
   }
 }
